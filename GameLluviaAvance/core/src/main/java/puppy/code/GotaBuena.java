965e2a31ed5040487d7a24445584758eb23f267b
BreepaYeah <146591637+Cboyeah@users.noreply.github.com>
2024-10-31 22:23:44 -0300

    Delete GameLluviaAvance directory
    Esta eliminación se realiza debido a que los archivos se encuentran en el rar

--- GameLluviaAvance/core/src/main/java/puppy/code/GotaBuena.java
+++
@@ -1,7 +0,0 @@
-package puppy.code;
-
-public class GotaBuena extends Gota{
-    public void efectoEnTarro(Tarro tarro){
-
-    }
-}
--- GameLluviaAvance/core/src/main/java/puppy/code/GotaMala.java
+++
@@ -1,7 +0,0 @@
-package puppy.code;
-
-public class GotaMala extends Gota{
-    public void efectoEnTarro(Tarro tarro){
-
-    }
-}
